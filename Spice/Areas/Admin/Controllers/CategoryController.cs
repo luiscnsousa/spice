dee21622840b36864a5815bef75921e2e5ad42d0
Luís Sousa <luiscnsousa@outlook.com>
2019-08-18 14:47:15 +0100

    images and css

---
+++ Spice/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿namespace Spice.Areas.Admin.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+    using Spice.Data;
+    using System.Threading.Tasks;
+
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        // GET action method
+        public async Task<IActionResult> Index()
+        {
+            return View(await this.db.Category.ToListAsync());
+        }
+    }
+}
