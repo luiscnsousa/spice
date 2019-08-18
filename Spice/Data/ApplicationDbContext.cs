96cc4f9e6f281c44c506961165a0908596a3ea92
Luís Sousa <luiscnsousa@outlook.com>
2019-08-18 13:54:50 +0100

    add category model and migration

--- Spice/Data/ApplicationDbContext.cs
+++ Spice/Data/ApplicationDbContext.cs
@@ -1,16 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-
-namespace Spice.Data
+﻿namespace Spice.Data
 {
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+    using Spice.Models;
+
     public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+
+        public DbSet<Category> Category { get; set; }
     }
 }
