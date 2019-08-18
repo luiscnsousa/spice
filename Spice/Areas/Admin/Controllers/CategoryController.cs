f36748ad77650be236155e69b48eb46cd2f95050
Luís Sousa <luiscnsousa@outlook.com>
2019-08-18 21:57:56 +0100

    Categories CRUD

--- Spice/Areas/Admin/Controllers/CategoryController.cs
+++ Spice/Areas/Admin/Controllers/CategoryController.cs
@@ -3,6 +3,7 @@
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.EntityFrameworkCore;
     using Spice.Data;
+    using Spice.Models;
     using System.Threading.Tasks;
 
     [Area("Admin")]
@@ -18,7 +19,101 @@
         // GET action method
         public async Task<IActionResult> Index()
         {
-            return View(await this.db.Category.ToListAsync());
+            return this.View(await this.db.Category.ToListAsync());
+        }
+
+        // GET - CREATE
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+
+        // POST - CREATE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Category category)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.View(category);
+            }
+
+            this.db.Category.Add(category);
+            await this.db.SaveChangesAsync();
+
+            return this.RedirectToAction(nameof(this.Index));
+        }
+
+        // GET - EDIT
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (!id.HasValue)
+            {
+                return this.NotFound();
+            }
+
+            var category = await this.db.Category.FindAsync(id.Value);
+            if (category == null)
+            {
+                return this.NotFound();
+            }
+
+            return this.View(category);
+        }
+
+        // POST - EDIT
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Category category)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.View(category);
+            }
+
+            this.db.Update(category);
+            await this.db.SaveChangesAsync();
+
+            return this.RedirectToAction(nameof(this.Index));
+        }
+
+        // GET - DELETE
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (!id.HasValue)
+            {
+                return this.NotFound();
+            }
+
+            var category = await this.db.Category.FindAsync(id.Value);
+            if (category == null)
+            {
+                return this.NotFound();
+            }
+
+            return this.View(category);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int? id)
+        {
+            if (!id.HasValue)
+            {
+                return this.NotFound();
+            }
+
+            var category = await this.db.Category.FindAsync(id);
+
+            if (category == null)
+            {
+                return this.View();
+            }
+
+            this.db.Remove(category);
+            await this.db.SaveChangesAsync();
+
+            return this.RedirectToAction(nameof(this.Index));
         }
     }
 }
