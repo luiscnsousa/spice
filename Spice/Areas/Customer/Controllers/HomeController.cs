b5c04c2d83136fb3416181636d945414425282ea
Luís Sousa <luiscnsousa@outlook.com>
2019-08-21 21:52:39 +0100

    landing page

--- Spice/Areas/Customer/Controllers/HomeController.cs
+++ Spice/Areas/Customer/Controllers/HomeController.cs
@@ -1,15 +1,37 @@
-﻿using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
-using Spice.Models;
-
-namespace Spice.Controllers
+﻿namespace Spice.Controllers
 {
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+    using Spice.Data;
+    using Spice.Models;
+    using Spice.Models.ViewModels;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Threading.Tasks;
+
     [Area("Customer")]
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext db;
+
+        public HomeController(ApplicationDbContext db)
         {
-            return View();
+            this.db = db;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var indexViewModel = new IndexViewModel
+            {
+                MenuItem = await this.db.MenuItem
+                    .Include(m => m.Category)
+                    .Include(m => m.SubCategory)
+                    .ToListAsync(),
+                Category = await this.db.Category.ToListAsync(),
+                Coupon = await this.db.Coupon.Where(c => c.IsActive).ToListAsync()
+            };
+
+            return View(indexViewModel);
         }
 
         public IActionResult Privacy()
